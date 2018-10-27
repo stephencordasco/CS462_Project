68bf7313dc47e014df3c94e89cbf9419f8a14d38
stephencordasco <stephencordasco@gmail.com>
2018-10-26 21:18:29 -0700

    Implemented functionality for purchase subscription. - Added a stub so that after data is entered, a payment is successful. - After a successful payment, user is taken back to the account menu.

--- Tuffy_Tetris/Tuffy_Tetris/UI/Windows/UI_console_win.cpp
+++ Tuffy_Tetris/Tuffy_Tetris/UI/Windows/UI_console_win.cpp
@@ -6,7 +6,10 @@ using UI::UI_console_win;
 
 UI_console_win::UI_console_win()
 {
-
+	username = " ";
+	password = " ";
+	email = " ";
+	hasSubscription = false;
 }
 
 // displays the first menu on application startup
@@ -112,6 +115,43 @@ void UI_console_win::displayAccountMenu() const
 	cout << "============================\n";
 }
 
+// displays the purchase subscription screen
+//  going to use a stub here so that any values entered will always be accepted
+//    and a subscription purchase will be successful
+void UI_console_win::displayPurchaseSubscriptionScreen() const
+{
+	// temp variables to hold the user input
+	string cardNumber = " ";
+	string accountHolder = " ";
+	string securityNumber = " ";
+	string expirationDate = " ";
+	string billingAddress = " ";
+	string zipCode = " ";
+
+	cout << "Card Number: ";
+	getline(cin, cardNumber);
+	cout << "Account Holder: ";
+	getline(cin, accountHolder);
+	cout << "Security Number: ";
+	getline(cin, securityNumber);
+	cout << "Expiration Date: ";
+	getline(cin, expirationDate);
+	cout << "Billing Address: ";
+	getline(cin, billingAddress);
+	cout << "Zip Code: ";
+	getline(cin, zipCode);
+
+	// stub: payment is successful, hasSubscription assigned true
+	cout << "\nPurchase successful!\n";
+	hasSubscription = true;
+
+	// if the payment was successful take the user back to the account menu
+	if (hasSubscription)
+	{
+		accountMenu();
+	}
+}
+
 // returns true if the user selected "Change Username", "Change Password", or "Purchase Subscription";
 //  returns false otherwise
 bool UI_console_win::accountMenu()
@@ -133,7 +173,7 @@ bool UI_console_win::accountMenu()
 
 		case '3':	// user chose to purchase subscription
 			// print the subscription screen
-
+			displayPurchaseSubscriptionScreen();
 			break;
 
 		case '4':	// user chose to return to main menu
@@ -144,17 +184,6 @@ bool UI_console_win::accountMenu()
 			cout << "\nINVALID MENU CHOICE\n";
 	}
 	return true;
-}
-
-// displays the purchase subscription screen
-void UI_console_win::displayPurchaseSubscriptionScreen() const
-{
-	cout << "Card Number: ";
-	cout << "Account Holder: ";
-	cout << "Security Number: ";
-	cout << "Expiration Date: ";
-	cout << "Billing Address: ";
-	cout << "Zip Code: ";
 }
 
 // gets user input for a menu selection
