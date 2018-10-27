be0bc15679eac7347939046d1296e83311df7ed2
stephencordasco <stephencordasco@gmail.com>
2018-10-26 21:07:55 -0700

    Added more member functions to UI_console_win. - Added more functionality to menus - Added many comments for clarity

--- Tuffy_Tetris/Tuffy_Tetris/UI/UI_console.h
+++ Tuffy_Tetris/Tuffy_Tetris/UI/UI_console.h
@@ -10,19 +10,30 @@ namespace UI
 	public:
 		UI_console();
 
-		//virtual methods
+		// printing menus to screen
 		virtual void displayLoginScreen() const = 0;
 		virtual void displayMainMenu() const = 0;
+		virtual void displayAccountMenu() const = 0;
+		virtual void displayPurchaseSubscriptionScreen() const = 0;
+
+		// menu functionality
 		virtual bool login() = 0;
+		virtual bool mainMenu() = 0;
+		virtual bool accountMenu() = 0;
+
+		// get user data
 		virtual char getMenuChoice() = 0;
 		virtual string getUserName() = 0;
 		virtual string getPassword() = 0;
 		virtual string getEmail() = 0;
+
 	private:
 		//common fields
 		char menuChoice;
 		string username;
 		string password;
 		string email;
+		bool hasSubscription;
+
 	};
 }
--- Tuffy_Tetris/Tuffy_Tetris/UI/Windows/UI_console_win.cpp
+++ Tuffy_Tetris/Tuffy_Tetris/UI/Windows/UI_console_win.cpp
@@ -9,6 +9,7 @@ UI_console_win::UI_console_win()
 
 }
 
+// displays the first menu on application startup
 void UI_console_win::displayLoginScreen() const
 {
 	cout << "============================\n";
@@ -18,6 +19,7 @@ void UI_console_win::displayLoginScreen() const
 	cout << "============================\n";
 }
 
+// displays a new menu on "Login" selection
 void displayLogin()
 {
 	cout << "Username: ";
@@ -25,6 +27,7 @@ void displayLogin()
 	cout << "email: ";
 }
 
+// displays a new menu on "Register" selection
 void displayRegister()
 {
 	cout << "Please fill in the following fields:\n";
@@ -33,28 +36,34 @@ void displayRegister()
 	cout << "\tCSUF email: ";
 }
 
+// returns true if the user selected "Login" or "Register"; returns false otherwise
 bool UI_console_win::login()
 {
+	// print the first menu
 	displayLoginScreen();
-	switch (menuChoice)
+
+	// store the menu choice
+	char choice = getMenuChoice();
+	switch (choice)
 	{
-		case '1':
+		case '1':	// user chose to login
 			displayLogin();
 			break;
 
-		case '2':
+		case '2':	// user chose to register
 			displayRegister();
 			break;
 
-		case '3':
+		case '3':	// user chose to quit
 			return false;
 
-		default:
+		default:	// user entered an invalid key
 			cout << "\nINVALID MENU CHOICE\n";
 	}
 	return true;
 }
 
+// displays the main menu after user has logged in successfully or registered an account successfully
 void UI_console_win::displayMainMenu() const
 {
 	cout << "============================\n";
@@ -64,21 +73,109 @@ void UI_console_win::displayMainMenu() const
 	cout << "============================\n";
 }
 
+// returns true if the user selected "Play Game" or "Account Information"; returns false otherwise
+bool UI_console_win::mainMenu()
+{
+	// print the main menu
+	displayMainMenu();
+
+	// store the menu choice
+	char choice = getMenuChoice();
+	switch (choice)
+	{
+		case '1':	// user chose to play game
+			// call start_game?
+			break;
+
+		case '2':	// user chose to access account information
+			// display account menu
+
+			break;
+			
+		case '3':	// user chose to quit
+			return false;
+
+		default:	// user entered an invalid key
+			cout << "\nINVALID MENU CHOICE\n";
+	}
+	return true;
+}
+
+// displays the account menu
+void UI_console_win::displayAccountMenu() const
+{
+	cout << "============================\n";
+	cout << "1.) Change Username\n";
+	cout << "2.) Change Password\n";
+	cout << "3.) Purchase Subscription\n";
+	cout << "4.) Main Menu\n";
+	cout << "============================\n";
+}
+
+// returns true if the user selected "Change Username", "Change Password", or "Purchase Subscription";
+//  returns false otherwise
+bool UI_console_win::accountMenu()
+{
+	// print the account menu
+	displayAccountMenu();
+
+	// store the menu choice
+	char choice = getMenuChoice();
+	switch (choice)
+	{
+		case '1':	// user chose to change username
+			// print the change username screen
+			break;
+
+		case '2':	// user chose to change password
+			// print the change password screen
+			break;
+
+		case '3':	// user chose to purchase subscription
+			// print the subscription screen
+
+			break;
+
+		case '4':	// user chose to return to main menu
+			return false;
+			break;
+
+		default:
+			cout << "\nINVALID MENU CHOICE\n";
+	}
+	return true;
+}
+
+// displays the purchase subscription screen
+void UI_console_win::displayPurchaseSubscriptionScreen() const
+{
+	cout << "Card Number: ";
+	cout << "Account Holder: ";
+	cout << "Security Number: ";
+	cout << "Expiration Date: ";
+	cout << "Billing Address: ";
+	cout << "Zip Code: ";
+}
+
+// gets user input for a menu selection
 char UI_console_win::getMenuChoice()
 {
 	cin >> menuChoice;
 }
 
+// stores the username: (Might not need?)
 string UI_console_win::getUserName()
 {
 	getline(cin, username);
 }
 
+// stores the password: (Might not need?)
 string UI_console_win::getPassword()
 {
 	getline(cin, password);
 }
 
+// stores the email: (Might not need?)
 string UI_console_win::getEmail()
 {
 	getline(cin, email);
