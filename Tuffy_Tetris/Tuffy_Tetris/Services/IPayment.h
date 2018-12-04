0ec437e741587f86a35f279f66d316862ccb9f8d
Ryanoune <ryanoune@gmail.com>
2018-12-03 23:12:18 -0800

    Added implementation to payment files

--- Tuffy_Tetris/Tuffy_Tetris/Services/IPayment.h
+++ Tuffy_Tetris/Tuffy_Tetris/Services/IPayment.h
@@ -3,12 +3,26 @@
 #ifndef IPAYMENT_H
 #define IPAYMENT_H
 
+#include<string>
+
 namespace Services 
 {
 	class IPayment 
 	{
-		
+	public:
+		IPayment();
+		virtual bool acceptCardInfo(std::string, std::string, std::string, std::string, std::string, std::string) = 0;
+		virtual void printToLogger(std::string) = 0;
+
+
 	};
+
+
+	inline IPayment::IPayment()
+	{}
+
+
+
 }
 
 
--- Tuffy_Tetris/Tuffy_Tetris/Services/Payment_VISA.cpp
+++ Tuffy_Tetris/Tuffy_Tetris/Services/Payment_VISA.cpp
@@ -7,13 +7,12 @@ Payment_VISA::Payment_VISA()
 
 }
 
-bool Payment_VISA::acceptCardInfo()
+bool Payment_VISA::acceptCardInfo(std::string cardNumber, std::string accountHolder, std::string securityNumber, std::string expirationDate, std::string billingAddress, std::string zipCode)
 {
-
 	return true;
 }
 
-void Payment_VISA::printToLogger()
+void Payment_VISA::printToLogger(std::string message)
 {
-
+	_logger << message;
 }
